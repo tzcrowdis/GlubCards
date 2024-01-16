8515b4ed2865e3ad5b2a2bf365325863b6d026a7
cowbr <cowbr@DESKTOP-VE9Q3AE>
2024-01-16 16:36:36 -0500

    Added void piece

--- Assets/PieceScripts/Pawn.cs
+++ Assets/PieceScripts/Pawn.cs
@@ -15,9 +15,6 @@ public class Pawn : PieceScript
         dmg = 1f;
         height = 0.5f; //based on model USE SETTER???
 
-        Debug.Log($"Child script is {GetType()}");
-        Debug.Log($"Parent script is {GetType().BaseType}");
-
         //Use base to call parent functions so generics get assigned
         base.Start();
     }
---
+++ Assets/PieceScripts/TheVoid.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class TheVoid : PieceScript
+{
+    MeshRenderer mesh;
+    public override void Start() 
+    {
+        //set any unique variables here
+        hp = 1f;
+        dmg = 1f;
+        height = 0.5f;
+
+        mesh = GetComponent<MeshRenderer>();
+
+        //Use base to call parent functions so generics get assigned
+        base.Start();
+    }
+
+    public override IEnumerator Move() 
+    {
+        moving = true;
+
+        GameObject[,] board = GameMaster.Instance.board;
+
+        //Get random position on board
+        int destX = UnityEngine.Random.Range(0, board.GetLength(0));
+        int destY = UnityEngine.Random.Range(0, board.GetLength(1));
+
+        //start fade out
+        float t = 0f;
+        Color colorTemp = mesh.material.color;
+
+        while (t < 1)
+        {
+            t = LerpAlpha(colorTemp, t, 1f, 0f);
+            yield return null;
+        }
+
+        LerpAlpha(colorTemp, 0, 0f, 0f);
+        t = 0;
+
+        while (t < 1)
+        {
+            t = LerpAlpha(colorTemp, t, 0f, 1f);
+            yield return null;
+        }
+
+        LerpAlpha(colorTemp, 1, 1f, 1f);
+
+        yield return null;
+    }
+
+    public float LerpAlpha(Color colorTemp, float t, float from, float to)
+    {
+        colorTemp.a = Mathf.Lerp(from, to, t);
+        mesh.material.color = colorTemp;
+        return t + Time.deltaTime;
+    }
+
+    public override void Attack(GameObject enemyPiece)
+    {
+        try
+        {
+            enemyPiece.GetComponent<PieceScript>().Defend(gameObject);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public override void Defend(GameObject enemy)
+    {
+        try
+        {
+            hp -= enemy.GetComponent<PieceScript>().dmg;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
