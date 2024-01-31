7756d898557ecdda073bd0b3882da1a74f10b32e
tzcrowdis <tcrowdis@umass.edu>
2024-01-31 12:35:49 -0700

    some brain leech progress

--- Assets/GameplayScripts/AttachmentScripts/BrainLeech.cs
+++ Assets/GameplayScripts/AttachmentScripts/BrainLeech.cs
@@ -6,6 +6,13 @@ public class BrainLeech : MonoBehaviour
 {
     GameObject suckee;
 
+    ParticleSystem blood;
+
+    private void Start()
+    {
+        blood = GetComponentInChildren<ParticleSystem>();
+    }
+
     //attached to piece by brain leech wizard
     //kills that piece once they reach their opponent
     public void Attach(GameObject piece)
@@ -14,6 +21,7 @@ public class BrainLeech : MonoBehaviour
 
         //change suckee's allegiance (flip 180 and set enemyPiece = !enemyPiece)
         suckee.GetComponent<PieceScript>().enemyPiece = !suckee.GetComponent<PieceScript>().enemyPiece;
+        //suckee.transform.Rotate(0f, 180f, 0f);
 
         switch (piece.name.Remove(piece.name.Length - 7))
         {
@@ -21,7 +29,6 @@ public class BrainLeech : MonoBehaviour
                 transform.parent = piece.transform;
                 transform.localPosition = new Vector3(0.6f, 1.3f, -0.12f);
                 transform.localRotation = Quaternion.Euler(-90, -90, 0);
-                //add blood spurt???
                 break;
             case "WW1Dog":
                 Debug.Log("dog found");
@@ -33,6 +40,9 @@ public class BrainLeech : MonoBehaviour
                 Debug.Log("void found");
                 break;
         }
+
+        //add blood spurt
+        StartCoroutine(BloodSpurt());
     }
 
     void Update()
@@ -58,6 +68,15 @@ public class BrainLeech : MonoBehaviour
 
         GameMaster.Instance.RemovePieceFromBoard(victim);
         Destroy(victim, 0f);
+
+        yield return null;
+    }
+
+    IEnumerator BloodSpurt()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Destroy(blood);
 
         yield return null;
     }
